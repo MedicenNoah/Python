796f359d5afa5861f6f4473c1b07019b20f88dca
MedicenNoah <noej.salazar@gmail.com>
2024-04-26 14:29:23 -0400

    .

--- SSH_REMOTE_COMMANDS.py
+++ SSH_REMOTE_COMMANDS.py
@@ -1,23 +1,17 @@
 # import libraries
 
 import paramiko
-import os
 
 # Vars
 
 Distribution = "IP"
 USER = "user"
-Connect = USER + "@" + Distribution
 OpenSRC = "ibm i open source path"
 ScriptName = "prueba.py"
 PathSRC = "path python py"
 Execute = PathSRC + ScriptName
 Remote_cmd = 'cd %s' % OpenSRC
 Remote_py = 'python3 %s' % Execute
-LogPath = os.environ.get("USERPROFILE")
-LogPath = LogPath + "\\Desktop"
-NameLog = "Prueba.txt"
-AllPathLog = LogPath + "\\" + NameLog
 Pass = "pass"
 
 # Execute SSH, Set Path and Extract Log User for SBS.
